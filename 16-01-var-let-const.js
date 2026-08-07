41c6abac287d07d7d4b2d92fbd7aa1e7c031591a
Aminul islam <aminulislam424842@gmail.com>
2026-08-07 11:50:02 +0600

    feat: task 01 - implement var, let, const examples

---
+++ 16-01-var-let-const.js
@@ -0,0 +1,7 @@
+var name = "Aminul Islam Mahi";
+let roll = 819693;
+const dateOfBirth = "6/5/2026";
+
+console.log(`Name : ${name}`);
+console.log(`Roll : ${roll}`);
+console.log(`Date Of Birth : ${dateOfBirth}`);
